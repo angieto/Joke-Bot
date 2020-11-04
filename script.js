ab31646ba2afa6f876c5c072f6c208bfa0e726f2
Angie To <angie.to@berkeley.edu>
2020-11-03 21:51:27 -0800

    working joke bot

---
+++ script.js
@@ -0,0 +1,46 @@
+const button = document.getElementById("btn")
+const audioElement = document.getElementById("audio")
+
+// Disable/Enable Button
+function toggleButton() {
+    button.disabled = !button.disabled
+}
+
+async function getJokes() {
+    let joke = ""
+    const apiUrl = `https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist`
+    try {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`
+        } else {
+            joke = data.joke
+        }
+        // Passing Joke to VoiceRSS API
+        tellMe(joke)
+        // Disable Button
+        toggleButton()
+    } catch (error) {
+        console.log("whoops", error)
+    }
+}
+
+async function tellMe(joke) {
+    const jokeString = joke.trim().replace(/ /g, "%20")
+    const key = config.KEY
+
+    VoiceRSS.speech({
+        key,
+        src: jokeString,
+        hl: "en-us",
+        r: 0,
+        c: "mp3",
+        f: "44khz_16bit_stereo",
+        ssml: false,
+    })
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes)
+audioElement.addEventListener("ended", toggleButton)
